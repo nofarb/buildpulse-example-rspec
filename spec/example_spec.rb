f452e1defebb73d35378fb5e99130e43c474a40e
nofarb <nofarb@gmail.com>
2025-06-04 11:23:35 -0500

    Update example_spec.rb

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -20,6 +20,10 @@ describe 'Example' do
     expect(true).to eq true
   end
 
+  it 'passes6' do
+    expect(true).to eq true
+  end
+
   it 'is flaky' do
     # 25% chance to fail, 75% to pass
     expect(rand < 0.75).to eq true
